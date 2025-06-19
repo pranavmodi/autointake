735b6d7a0c828bd6626ba00c5eca79a3af46c375
Pranav Modi <pranav.modi@gmail.com>
2025-06-19 09:59:48 +0000

    update port info

--- autointake/main.py
+++ autointake/main.py
@@ -1,4 +1,8 @@
 import uvicorn
+import os
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True) 
+    load_dotenv()
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True) 
